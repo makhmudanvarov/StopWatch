c26ba6bba35eaef8df9115b96bc48f385f78d3b6
Makhmud <fordailyuse0108@gmail.com>
2025-09-26 17:28:00 +0500

    watch done, stopwatch half made

---
+++ js/script.js
@@ -0,0 +1,90 @@
+// Soatni ishlatish
+
+const secArr = document.querySelector(".s"),
+  minArr = document.querySelector(".m"),
+  hourArr = document.querySelector(".h");
+
+const hourNum = document.querySelector(".hours"),
+  minNum = document.querySelector(".minutes");
+
+function clock() {
+  let time = new Date(),
+    seconds = time.getSeconds(),
+    minutes = time.getMinutes(),
+    hours = time.getHours();
+
+  secArr.style.transform = `rotate(${seconds * 6}deg)`;
+  minArr.style.transform = `rotate(${minutes * 6}deg)`;
+  hourArr.style.transform = `rotate(${hours * 30}deg)`;
+
+  hourNum.innerHTML =
+    time.getHours() < 10 ? `0${time.getHours()}` : time.getHours();
+  minNum.innerHTML =
+    time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes();
+
+  setTimeout(() => {
+    clock();
+  }, 1000);
+}
+
+clock();
+
+// soat dan skundomerga o'tish
+
+const tabsItems = document.querySelectorAll(".tabsItem"),
+  tabsContentItem = document.querySelectorAll(".tabsContentItem");
+
+tabsItems.forEach((tab, i) => {
+  tab.addEventListener("click", () => {
+    tabsItems.forEach((item, j) => {
+      item.classList.remove("active");
+      tabsContentItem[j].classList.remove("active");
+    });
+    tabsContentItem[i].classList.add("active");
+    tab.classList.add("active");
+  });
+});
+
+// Sekundomer
+
+const stopBtn = document.querySelector(".stopwatch__btn"),
+  stopWatchSec = document.querySelector(".stopwatch__seconds"),
+  stopWatchMin = document.querySelector(".stopwatch__minutes"),
+  stopWatchHour = document.querySelector(".stopwatch__hours");
+
+stopBtn.innerHTML = "Start";
+let seconds2 = 0;
+let minutes2 = 0;
+let hours2 = 0;
+
+
+stopBtn.addEventListener("click", () => {
+  if (stopBtn.innerHTML == "Start") {
+    stopBtn.innerHTML = "Stop";
+    countSec();
+  } else if (stopBtn.innerHTML == "Stop") {
+    stopBtn.innerHTML = "Clear";
+  } else if (stopBtn.innerHTML == "Clear") {
+    stopBtn.innerHTML = "Start";
+  }
+});
+
+function countSec() {
+  if (seconds2 == 60) {
+    seconds2 = 0;
+    minutes2++;
+    stopWatchMin.innerHTML = minutes2;
+  } 
+  if (minutes2 == 60) {
+    minutes2 = 0;
+    hours2++
+    stopWatchHour.innerHTML = hours2;
+  }
+  stopWatchSec.innerHTML = seconds2;
+  seconds2++;
+
+  setTimeout(() => {
+    countSec();
+  }, 1000);
+  
+}
